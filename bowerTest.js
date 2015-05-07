3612599b93b5caab83f8b093c1a61df3cbd7dd98
Miguel Vizcaino <miguel87831@gmail.com>
2015-05-07 07:20:35 -0430

    feature/MIX-1 create methods to manage api bower, and create method to install package in the server.

--- bowerTest.js
+++ bowerTest.js
@@ -1,16 +1,79 @@
 var bower = require('bower');
+var Q = require('q');
+var exports = module.exports = {};
 
-var archivo = 'underscore';
+function isEmptyJSON(obj) {
+	var name;
 
-bower.commands
-	.install([archivo], { save: true }, { /* custom config */ })
-	.on('end', function (installed) {
-		console.log("Archivo instalado");
-		console.log(installed);
-	    console.log(installed[archivo].canonicalDir + "/" + installed[archivo].pkgMeta.main);
-	})
-	.on('error' , function (err) {
-		if (err.code === "ENOTFOUND") {
-			console.log("Archivo no encontrado");
-		}
-	});
+ 	for(name in obj) {
+ 		return false; 
+ 	}
+
+  	return true;
+}
+
+exports.installPackage = function(fileName) {
+	var deferred = Q.defer();
+
+	bower.commands
+		.install([fileName], { save: true }, {/* custom config*/})
+		.on('end', function (result) {
+			deferred.resolve(true);
+		})
+		.on('error', function (err) {
+			if (err.code === "ENOTFOUND") {
+				deferred.reject("Archivo no encontrado"); 				
+ 			}
+		})
+
+	return deferred.promise;
+};
+
+exports.listPackage = function() {
+	var deferred = Q.defer();
+
+	bower.commands
+		.list({paths:true})
+		.on('end', function (data) {
+
+			deferred.resolve(data);
+			
+		})
+		.on('error', function (err) {
+			deferred.reject("error");
+		})
+
+	return deferred.promise;
+};
+
+exports.searchPackage = function(name) {
+	var deferred = Q.defer();
+
+	bower.commands
+		.list({paths:true})
+		.on('end', function (data) {
+			if(Object.keys(data).lenght > 0) {
+				Object.keys(data).forEach(function(p) {
+					if(name === p) {
+						deferred.resolve
+						(
+							{
+								'exist':true,
+								'name':p,
+								'path':data[p]
+							}
+						);
+					} else {
+						deferred.resolve(false);
+					}	
+				});
+			}else {
+				deferred.resolve(false);
+			} 
+		})
+		.on('error', function (err) {
+			deferred.reject("error");
+		})
+
+	return deferred.promise;
+};
--- server.js
+++ server.js
@@ -1,6 +1,48 @@
 var express = require('express');
+var apiBower = require('./bowerTest.js');
+var Q = require('q');
+
 var app = express(); 
 
+var object = { 'jquery':'last','underscore':'last','angular':'last', 'amplify':'last'}
+
+var installPackages = function(packages) {
+	var count,
+		promises;
+
+	count = Object.keys(packages).length;
+	promises = [];
+
+	if(count > 1) {
+		Object.keys(packages).forEach(function (val,index) {
+		var deferred = Q.defer();
+ 			apiBower.searchPackage(val)
+		 		.then(function (data) {
+	 			if(!data) {
+	 				apiBower.installPackage(val)
+	 					.then(function (data) {
+	 						apiBower.listPackage()
+	 							.then(function (data) {
+	 								deferred.resolve(data);
+	 							})
+	 					})
+	 			}
+	 		})
+		 	promises.push(deferred.promise);
+	 	});
+
+	 	return Q.all(promises);
+	}
+}
+
+installPackages(object).then(function (data) {
+	var count,
+		object;
+
+	count = Object.keys(data).length;
+	console.log(data[0]);
+});
+	
 var html_dir = 'js/archivo.js';
 
 app.get('/', function(req, res) {
